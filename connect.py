8038353595d6173668862fab58e856f4c0119e2f
Lucas Diniz <lucas.diniz@scivisum.co.uk>
2018-11-23 23:53:17 +0000

    A bit more reliable now
    *We now ensure the path exists at module level
    *Before trying to mount kill all connected processes and umount
     (In cases where you might have forgotten something mounted there
      already, or a broken process)
    * Same for when we leave.
    * Still very rough, but that's what you get when you commit
    at midnight (without coffee)

--- connect.py
+++ connect.py
@@ -1,7 +1,18 @@
 import sh
 import click
 
-TMP_MOUNT_POINT = '/tmp/test'
+TMP_PATH = '/tmp/remote'
+sh.mkdir('-p', TMP_PATH)
+
+
+def ensure_safe(f):
+    def inner(*args):
+        try:
+            sh.fuser('-k', TMP_PATH)
+            sh.fusermount3('-u', TMP_PATH)
+        except sh.ErrorReturnCode_1:
+            f(*args)
+    return inner
 
 
 class RemoteMount(object):
@@ -13,11 +24,13 @@ class RemoteMount(object):
     def __enter__(self):
         return self
 
+    @ensure_safe
     def __exit__(self, *args):
-        sh.fusermount3('-u', TMP_MOUNT_POINT)
+        pass
 
+    @ensure_safe
     def mount(self):
-        sh.sshfs('{}:{}'.format(self.server, self.repo), TMP_MOUNT_POINT)
+        sh.sshfs('{}:{}'.format(self.server, self.repo), TMP_PATH)
 
 
 @click.command()
@@ -28,13 +41,10 @@ def main(server, repo):
     server:        A box to connect to.
     """
     with RemoteMount(server, repo) as r:
-        print "Mounting {}'s {} repo at {}".format(server, repo, TMP_MOUNT_POINT)
+        print "Mounting {}'s {} repo at {}".format(server, repo, TMP_PATH)
         r.mount()
-        raw_input("Press any key to exit and unmount...")
+        raw_input("Press any key to exit and attempt to unmount...")
 
 
 if __name__ == '__main__':
     main()
-
-# TODO: Handle potential exceptions thrown by sshfs and fusermount
-# TODO: Find a better way to wait/halt execution
